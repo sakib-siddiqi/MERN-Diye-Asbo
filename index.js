fce0d98595a5c42ec82c6a8f6980cf4adbd505c5
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-10-31 13:14:16 +0600

    get & post service + get and post orders

--- index.js
+++ index.js
@@ -14,7 +14,49 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${p
 const client = new MongoClient(uri);
 (async function () {
   try {
-  } catch (err) {}
+    /**
+     * connect client
+     */
+    await client.connect();
+    /**
+     * db and collection
+     */
+    const DB_diye_asbo = client.db("Diye-asbo");
+    const Services = DB_diye_asbo.collection("Services");
+    const AllBookings = DB_diye_asbo.collection("AllBookings");
+    /**
+     * get Service
+     */
+    app.get("/services", async (req, res) => {
+      const result = await Services.find({}).toArray();
+      res.json(result);
+    });
+    /**
+     * Post Service
+     */
+    app.post("/services", async (req, res) => {
+      const serviceData = req.body;
+      const result = await Services.insertOne(serviceData);
+      res.json(result);
+    });
+    /**
+     * Post Bookings
+     */
+    app.post("/bookings", async (req, res) => {
+      const BookingsData = req.body;
+      const result = await AllBookings.insertOne(BookingsData);
+      res.json(result);
+    });
+    /**
+     * get Bookings
+     */
+    app.get("/bookings", async (req, res) => {
+      const result = await AllBookings.find({}).toArray();
+      res.json(result);
+    });
+  } catch (err) {
+    console.log(err.code);
+  }
 })();
 app.get("/", (req, res) => res.send("server live now"));
 app.listen(port, () => console.log("running"));
